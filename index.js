ea2f88df24b036d9be71ce5561a0bf930f8e9a70
Basuraj <basurajp135@gmail.com>
2024-02-06 21:32:31 +0530

    user json data added

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
-require('dotenv').config()
+require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT
+const port = process.env.PORT;
+
+const userData = {
+  id: 1,
+  name: "Leanne Graham",
+  username: "Bret",
+  email: "Sincere@april.biz",
+  address: {
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998-3874",
+    geo: {
+      lat: "-37.3159",
+      lng: "81.1496",
+    },
+  },
+};
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -15,6 +32,10 @@ app.get("/twitter", (req, res) => {
 
 app.get("/login", (req, res) => {
   res.send("<h1>This is login route </h1>");
+});
+
+app.get("/user", (req, res) => {
+  res.json(userData);
 });
 
 app.listen(port, () => {
